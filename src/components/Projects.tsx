06c2a3137af22e1b26f4c3fdcd3d1583e524a5a8
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2023-11-01 21:30:30 -0400

    added projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import ProjectCard from "./ProjectCard";
 import CryptoViewerImg from '../assets/CryptoViewer.png';
+import HackathonImg from '../assets/Hackathon.png';
 import '../styles/projects.scss';
 
 export default function Projects() {
@@ -10,33 +11,30 @@ export default function Projects() {
         className='card'
         img={CryptoViewerImg}
         title="Crypto Price Viewer" 
-        desc="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+        desc="
+          Using CoinMarketCap's API, the crypto price viewer website displays the top 200 cryptocurrencies by market cap in a table. The table 
+          features a search bar to filter the results from the table, and pagination to page through 10 cryptos at a time. Although not currently
+          deployed for cost reasons, the frontend was deployed to netlify and the api was deployed to railway. There is also a light/dark mode toggle
+          for the page.
+        "
         alt="Crypto Price Viewer Website"
         url="https://github.com/malabudi/AssetPriceViewer"
+        builtWith='AngularJS, NestJS, NX, TypeScript'
         />
+
         <ProjectCard
         className='card'
-        img={CryptoViewerImg}
-        title="Crypto Price Viewer" 
-        desc="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-        alt="Crypto Price Viewer Website"
+        img={HackathonImg}
+        title="Study Buddy - Hackathon Project"
+        desc="
+          During Fall of 2023, I worked with three people in a hackathon called HackDearborn to build this website. Using OpenAi, a user enters a prompt
+          which will be their topic for the AI to generate. Using the topic OpenAI will generate flashcards for the user and display them on a seperate page.
+          Using the flashcards, it gets fed back into OpenAI's API to generate quiz questions also for the user. The user can then study off the flashcards generated
+          for them and quiz on the flashcards as much as they like.
+        "
+        alt="Hackathon Project from HackDearborn"
         url="https://github.com/malabudi/AssetPriceViewer"
-        />
-        <ProjectCard
-        className='card'
-        img={CryptoViewerImg}
-        title="Crypto Price Viewer" 
-        desc="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-        alt="Crypto Price Viewer Website"
-        url="https://github.com/malabudi/AssetPriceViewer"
-        />
-        <ProjectCard
-        className='card'
-        img={CryptoViewerImg}
-        title="Crypto Price Viewer" 
-        desc="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-        alt="Crypto Price Viewer Website"
-        url="https://github.com/malabudi/AssetPriceViewer"
+        builtWith='ReactJS, Flask, TypeScript, Python'
         />
       </div>
     </div>
